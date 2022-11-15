9a26fad6e5f46a8773e9034ee91a519c4ec392fd
Asgardpuper <maks.kachalay@gmail.com>
2022-11-15 15:39:38 +0200

    Fix 3 tast, add console log for data

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,32 +1,33 @@
-import lodash from "lodash.throttle";
+import lodash from 'lodash.throttle';
 
-const FEEDBACK_DATA = "feedback-form-state";
+const FEEDBACK_DATA = 'feedback-form-state';
 
-const form = document.querySelector(".feedback-form");
+const form = document.querySelector('.feedback-form');
 
-const inputEl = document.querySelector(".feedback-form input");
-const textareaEl = document.querySelector(".feedback-form textarea");
+const inputEl = document.querySelector('.feedback-form input');
+const textareaEl = document.querySelector('.feedback-form textarea');
 
-form.addEventListener("input", lodash(onInputChange, 500));
-form.addEventListener("submit", onFormSubmit);
+form.addEventListener('input', lodash(onInputChange, 500));
+form.addEventListener('submit', onFormSubmit);
 
 function onInputChange(e) {
-	const inputData = {
-		email: inputEl.value,
-		message: textareaEl.value,
-	};
-	localStorage.setItem(FEEDBACK_DATA, JSON.stringify(inputData));
+  const inputData = {
+    email: inputEl.value,
+    message: textareaEl.value,
+  };
+  localStorage.setItem(FEEDBACK_DATA, JSON.stringify(inputData));
 }
 
 function onFormSubmit(evt) {
-	evt.preventDefault();
-	const savedData = JSON.parse(localStorage.getItem(FEEDBACK_DATA));
-	form.reset();
-	localStorage.removeItem(FEEDBACK_DATA);
+  evt.preventDefault();
+  const savedData = JSON.parse(localStorage.getItem(FEEDBACK_DATA));
+  console.log(savedData);
+  form.reset();
+  localStorage.removeItem(FEEDBACK_DATA);
 }
 
 if (localStorage.getItem(FEEDBACK_DATA)) {
-	const outputData = JSON.parse(localStorage.getItem(FEEDBACK_DATA));
-	inputEl.value = outputData.email;
-	textareaEl.value = outputData.message;
+  const outputData = JSON.parse(localStorage.getItem(FEEDBACK_DATA));
+  inputEl.value = outputData.email;
+  textareaEl.value = outputData.message;
 }
